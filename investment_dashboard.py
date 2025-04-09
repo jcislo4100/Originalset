fe1f9ac0e3a4e0f400a1bcf3525d1807b342ad46
jcislo4100 <jcislo4100@gmail.com>
2025-04-08 22:50:08 -0400

    Update investment_dashboard.py

--- investment_dashboard.py
+++ investment_dashboard.py
@@ -136,10 +136,10 @@ if uploaded_file is not None:
 
             st.subheader(":moneybag: Capital Allocation by Fund")
             pie_df = df_filtered.groupby("Fund Name")["Cost"].sum().reset_index()
-fig3 = px.pie(pie_df, names="Fund Name", values="Cost", title="Capital Invested per Fund")
-st.plotly_chart(fig3, use_container_width=True)
+            fig3 = px.pie(pie_df, names="Fund Name", values="Cost", title="Capital Invested per Fund")
+            st.plotly_chart(fig3, use_container_width=True)
 
-if "Stage" in df_filtered.columns:
+            if "Stage" in df_filtered.columns:
                 st.subheader(":dna: Investments by Stage")
                 stage_df = df_filtered.groupby("Stage")["Cost"].sum().reset_index()
                 fig4 = px.pie(stage_df, names="Stage", values="Cost", title="Investments by Stage")
